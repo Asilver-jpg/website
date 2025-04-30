95958c76ee0691914d01ee44e6a60cad2cfe8071
Asilver-JPG <asilver7493@gmail.com>
2025-04-30 14:12:38 -0400

    build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps:{
-    include:['pagepiling.js']
-  }
+  build: {
+    outDir: 'dist', // or 'dist' depending on your setup
+  },
 })
