fe6a8155db946f715b6cf41a37e546cf477b99ff
Asilver-JPG <asilver7493@gmail.com>
2025-05-05 19:57:50 -0400

    Fixing build attempt 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist', // or 'dist' depending on your setup
